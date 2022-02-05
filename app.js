a4db98262d6929ef27e143269aa9bfee1663dd25
Renato Delboni <renatonato_jr@hotmail.com>
2022-02-05 19:23:34 +0900

    able to show/remove dropdown + background

--- app.js
+++ app.js
@@ -3,11 +3,16 @@ const background = document.querySelector('.dropdownBackground');
 const nav = document.querySelector('.top');
 
 function handleEnter() {
-  console.log('Enter');
+  this.classList.add('trigger-enter');
+  // the value of this is inherited from the function when using the arrow function
+  setTimeout(() => this.classList.add('trigger-enter-active'), 150);
+  background.classList.add('open');
 }
 
 function handleLeave() {
-  console.log('leave');
+  this.classList.remove('trigger-enter', 'trigger-enter-active');
+  background.classList.remove('open');
+  // console.log('leave');
 }
 
 triggers.forEach(trigger => trigger.addEventListener('mouseenter', handleEnter));
